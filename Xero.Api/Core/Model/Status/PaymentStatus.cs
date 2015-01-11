040c85c4a4eeb20479fe02efa0c3c7e43fbb8be3
csharpwizard <tysoncoder@gmail.com>
2015-01-12 10:38:47 +1300

    changed TokenKey to use the ConsumerKey

--- Xero.Api/Core/Model/Status/PaymentStatus.cs
+++ Xero.Api/Core/Model/Status/PaymentStatus.cs
@@ -5,8 +5,6 @@ namespace Xero.Api.Core.Model.Status
     public enum PaymentStatus
     {
         [EnumMember(Value = "AUTHORISED")]
-        Authorised,
-        [EnumMember(Value = "DELETED")]
-        Deleted
+        Authorised        
     }
 }
