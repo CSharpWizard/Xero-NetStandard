80685071f91d48c762c68e7e6b7816b105a9d5d1
csharpwizard <tysoncoder@gmail.com>
2015-09-11 17:14:59 +1200

    [WIP] LinkedTransactions. Basic framework for creating and updating linked transactions. Delete to be added, as well as example tests

--- CoreTests/Integration/Payments/PaymentsTest.cs
+++ CoreTests/Integration/Payments/PaymentsTest.cs
@@ -126,12 +126,5 @@ namespace CoreTests.Integration.Payments
                 }
             });
         }
-
-        protected void Given_this_payment_is_deleted(Payment payment)
-        {
-            var deleteThisPayment = new Payment { Status = PaymentStatus.Deleted, Id = payment.Id };
-
-            Api.Payments.Update(deleteThisPayment);
-        }
     }
 }
