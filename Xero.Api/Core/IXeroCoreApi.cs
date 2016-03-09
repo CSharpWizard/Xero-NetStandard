1aeb940d36b6415fb86738b4aa281ac1056f481a
csharpwizard <tysoncoder@gmail.com>
2016-03-10 11:08:15 +1300

    Update the purchase orders endpoint to be an update endpoint

--- Xero.Api/Core/IXeroCoreApi.cs
+++ Xero.Api/Core/IXeroCoreApi.cs
@@ -84,7 +84,5 @@ namespace Xero.Api.Core
         TaxRate Update(TaxRate item);
         ImportSummary Update(Setup item);
         TrackingCategory Update(TrackingCategory item);
-
-        void SummarizeErrors(bool summarize);
     }
 }
