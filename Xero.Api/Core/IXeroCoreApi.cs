224a0c5b8109d5531d42aab1ef3122181e3b217b
csharpwizard <tysoncoder@gmail.com>
2016-06-07 10:28:04 +1200

    Added OnlineInvoiceUrl functionality

--- Xero.Api/Core/IXeroCoreApi.cs
+++ Xero.Api/Core/IXeroCoreApi.cs
@@ -23,7 +23,6 @@ namespace Xero.Api.Core
         IFilesEndpoint Files { get; }
         IFoldersEndpoint Folders { get; }
         IInboxEndpoint Inbox { get; }
-        IAssociationsEndpoint Associations { get; }
         IInvoicesEndpoint Invoices { get; }
         IItemsEndpoint Items { get; }
         IJournalsEndpoint Journals { get; }
