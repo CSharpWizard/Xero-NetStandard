1ea65e977170242795cc2aad379b5ae70e13e189
csharpwizard <tysoncoder@gmail.com>
2017-07-07 11:05:13 +0100

    Updated Nuget dependency on Newtonsoft.Json

--- Xero.Api/Core/Endpoints/CreditNotesEndpoint.cs
+++ Xero.Api/Core/Endpoints/CreditNotesEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Xero.Api.Common;
-using Xero.Api.Core.Endpoints.Base;
+﻿using Xero.Api.Core.Endpoints.Base;
 using Xero.Api.Core.Model;
 using Xero.Api.Core.Request;
 using Xero.Api.Core.Response;
@@ -9,7 +7,7 @@ using Xero.Api.Infrastructure.Http;
 namespace Xero.Api.Core.Endpoints
 {
     public interface ICreditNotesEndpoint :
-        IXeroUpdateEndpoint<CreditNotesEndpoint, CreditNote, CreditNotesRequest, CreditNotesResponse>, IPageableEndpoint<ICreditNotesEndpoint>
+        IXeroUpdateEndpoint<CreditNotesEndpoint, CreditNote, CreditNotesRequest, CreditNotesResponse>
     {
         
     }
@@ -20,18 +18,6 @@ namespace Xero.Api.Core.Endpoints
         public CreditNotesEndpoint(XeroHttpClient client) :
             base(client, "/api.xro/2.0/CreditNotes")
         {
-        }
-
-        public ICreditNotesEndpoint Page(int page)
-        {
-            AddParameter("page", page);
-            return this;
-        }
-
-        public override void ClearQueryString()
-        {
-            base.ClearQueryString();
-            Page(1);
-        }
+        }        
     }
 }
