0467e487e510d6f6ea2a28d04c2db2ab285a7046
csharpwizard <tysoncoder@gmail.com>
2017-12-07 12:52:27 +1300

    Add functionality to subscribe organisation to other currencies

--- Xero.Api/Core/Model/TaxComponent.cs
+++ Xero.Api/Core/Model/TaxComponent.cs
@@ -14,8 +14,5 @@ namespace Xero.Api.Core.Model
 
         [DataMember(EmitDefaultValue = false)]
         public bool? IsCompound { get; set; }
-
-        [DataMember(EmitDefaultValue = false)]
-        public bool? IsNonRecoverable { get; set; }
     }
 }
