63cccb6e5200927c7e2eb586b8007eeea42fda88
csharpwizard <tysoncoder@gmail.com>
2022-09-13 16:16:38 +1000

    Restsharp version bump (#440)

--- Xero.NetStandard.OAuth2/Client/Configuration.cs
+++ Xero.NetStandard.OAuth2/Client/Configuration.cs
@@ -16,6 +16,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace Xero.NetStandard.OAuth2.Client
 {
@@ -30,7 +31,7 @@ namespace Xero.NetStandard.OAuth2.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "3.28.0";
+        public const string Version = "3.28.1";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -102,11 +103,12 @@ namespace Xero.NetStandard.OAuth2.Client
         [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "VirtualMemberCallInConstructor")]
         public Configuration()
         {
-            UserAgent = "xero-netstandard-3.28.0";
+            UserAgent = "xero-netstandard-3.28.1";
             BasePath = "https://api.xero.com/api.xro/2.0";
             DefaultHeader = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
             ApiKeyPrefix = new ConcurrentDictionary<string, string>();
+            Cookies = new List<Cookie>();
 
             // Setting Timeout has side effects (forces ApiClient creation).
             Timeout = 300000;
@@ -190,6 +192,11 @@ namespace Xero.NetStandard.OAuth2.Client
         /// </summary>
         /// <value>The password.</value>
         public virtual string Password { get; set; }
+
+        /// <summary>
+        /// Cookies to be sent along with the request.
+        /// </summary>
+        public virtual List<Cookie> Cookies { get; set; }
 
         /// <summary>
         /// Gets the API key with prefix.
@@ -335,7 +342,7 @@ namespace Xero.NetStandard.OAuth2.Client
             String report = "C# SDK (Xero.NetStandard.OAuth2) Debug Report:\n";
             report += "    OS: " + System.Runtime.InteropServices.RuntimeInformation.OSDescription + "\n";
             report += "    Version of the API: 2.29.2\n";
-            report += "    SDK Package Version: 3.28.0\n";
+            report += "    SDK Package Version: 3.28.1\n";
 
             return report;
         }
